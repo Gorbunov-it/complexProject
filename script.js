bff8ed0fc0e6b9b3c4e66d0adfeb9a5435e24d23
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-11-16 20:47:04 +0400

    Внес правки по lesson04

--- script.js
+++ script.js
@@ -4,10 +4,9 @@
 const _str = " Cоздайте функцию, которая принимает 1 аргумент (название произвольное) ";
 
 // 1) Создайте функцию, которая принимает 1 аргумент (название произвольное)
-const fun = function funValidStr(variable) {
+const fun = function (variable) {
   if (typeof variable === "string") {
-    variable = variable.trim();
-    return variable.length > 30 ? variable.slice(0, 30) + "..." : variable;
+    return variable.trim().length > 30 ? variable.slice(0, 30) + "..." : variable;
   } else {
     return "в качестве аргумента передана не строка";
   }
