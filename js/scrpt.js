cdf2f839d1d33cb588e6a49e3b564f18060a98fc
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-12-12 22:52:36 +0400

    Создал ветку lesson15

---
+++ js/scrpt.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// 1) Создать функцию-конструктор DomElement, который
+//    содержит свойства
+//   - selector,
+//   - height,
+//   - width,
+//   - bg,
+//   - fontSize
+// содержит метод, который создает элемент на странице в зависимости от условия:
+// - если строка selector начинается с точки, создаем div с классом
+// - если строка selector начинается с решетки # то создаем параграф с id
+// пример:
+// если передана строка '.block', то функция конструктор создает элемент с class="block"
+// если передана строка '#best', то функция конструктор создает элемент с id =best"
+// с помощью cssText задавать стили:
+//   - высотой - height,
+//   - шириной - width,
+//   - background - bg
+//   - размер текста fontSize
+// Внутрь созданного блока записывать любой текст. Метод записи может быть любым.
+// 2) Создать новый объект на основе класса DomElement
+// 3) Вызвать его метод чтобы создать элемент на странице
+
+class DomElement {
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.addDomElement = () => {
+      console.log(`Add Element to page`);
+    };
+  }
+}
+
+const newDomElment = new DomElement("block", "100px", "100px", "red");
+
+newDomElment.addDomElement();
