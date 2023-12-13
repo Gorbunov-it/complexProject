4c2718e2f699c6f2c456416a9fdbc766652f367f
Ivan Gorbunov <I.MGorbunov@yandex.ru>
2023-12-13 23:29:43 +0400

    lesson15 [задание 2]

--- js/scrpt.js
+++ js/scrpt.js
@@ -20,6 +20,31 @@ class DomElement {
     this.element.textContent = "Какой-то текст";
     body.append(this.element);
   }
+
+  addEventKeyDown = () => {
+    window.addEventListener("keydown", (e) => {
+      if (e.code == "ArrowUp") {
+        let goUp = this.element.offsetTop;
+        console.log(goUp);
+        this.element.style.top = goUp - 40 + "px";
+      }
+      if (e.code == "ArrowRight") {
+        let goRight = this.element.offsetLeft;
+        console.log("goRight: ", goRight);
+        this.element.style.left = goRight + 40 + "px";
+      }
+      if (e.code == "ArrowDown") {
+        let goDown = this.element.offsetTop;
+        console.log("goDown: ", goDown);
+        this.element.style.top = goDown + 40 + "px";
+      }
+      if (e.code == "ArrowLeft") {
+        let goLeft = this.element.offsetLeft;
+        console.log("goLeft: ", goLeft);
+        this.element.style.left = goLeft - 40 + "px";
+      }
+    });
+  };
 }
 
 const Init = () => {
@@ -30,6 +55,7 @@ const Init = () => {
 
   let element = new DomElement(selector, attribute, classElement, "250px", "250px", "blue", "12px");
   element.create();
+  element.addEventKeyDown();
 };
 
 Init();
